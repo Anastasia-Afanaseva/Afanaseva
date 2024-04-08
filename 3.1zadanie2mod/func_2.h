6b94a5e57b637155d6f270b345aa97c1467d6713
Афанасьева Анастасия <gerus9791@gmail.com>
2024-04-08 22:37:57 +0700

    udaleno

--- 3.1zadanie2mod/func_2.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNC_2_H_
-#define FUNC_2_H_
-
-int binary_to_number(char *str);
-int stat_func(char *file, char *number);
-
-#endif
--- 3.1zadanie2mod/func_3.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNC_3_H_
-#define FUNC_3_H_
-
-int check_change(char *str);
-void edit(char *str, char *doubl);
-void binary_to_letters(char *str);
-
-#endif
